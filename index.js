11dfad199a2e34030ac9f7faab48c51315d69ac1
Raúl Salas <rauls.martin@alumno.sek.es>
2017-07-08 11:45:47 +0200

    Starting project

--- index.js
+++ index.js
@@ -1,8 +1,62 @@
-const electron = require('electron')
-const {app, BrowserWindow} = electron
+const electron = require('electron');
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+const isAlreadyRunning = app.makeSingleInstance(() => {
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore();
+    }
+
+    mainWindow.show();
+  }
+});
+
+if (isAlreadyRunning) {
+  app.quit();
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
 
 app.on('ready', () => {
-  let win = new BrowserWindow({width:800, height: 600})
-  win.loadURL(`https://google.com`)
-  win.webContents.openDevTools()
-})
+  mainWindow = new BrowserWindow({
+    show: false,
+    title: app.getName(),
+    width: 500,
+    height: 600,
+    minWidth: 500,
+    minHeight: 600,
+    maxWidth: 500,
+    maxHeight: 600,
+    resizable: false,
+    fullscreen: false,
+    background: '#2196F3',
+    titleBarStyle: 'hiddenInset',
+    frame: process.platform == 'darwin',
+    autoHideMenuBar: true
+  });
+
+  if (process.platform === 'darwin') {
+    mainWindow.setSheetOffset(40);
+  }
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('enter-full-screen', e => {
+    e.preventDefault();
+  });
+
+  mainWindow.on('ready-to-show', () => {
+      mainWindow.show();
+      mainWindow.focus();
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
