9a84446224c9885e79b89fb15eda6a574c7adac0
KernelFreeze <kernelfreeze@outlook.com>
2017-08-17 09:07:34 -0500

    Convirtiendo a pug

--- index.js
+++ index.js
@@ -1,75 +1,84 @@
 'use strict';
 
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
 const ygg = require('yggdrasil')({});
+
+require('electron-pug')({
+    pretty: true
+});
+require('electron-reload')(__dirname);
+
+const {
+    app,
+    BrowserWindow
+} = electron;
 
 let mainWindow;
 
 const isAlreadyRunning = app.makeSingleInstance(() => {
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) {
-      mainWindow.restore();
-    }
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+            mainWindow.restore();
+        }
 
-    mainWindow.show();
-  }
+        mainWindow.show();
+    }
 });
 
 if (isAlreadyRunning) {
-  app.quit();
+    app.quit();
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    show: false,
-    title: app.getName(),
-    width: 500,
-    height: 600,
-    minWidth: 500,
-    minHeight: 600,
-    maxWidth: 500,
-    maxHeight: 600,
-    resizable: false,
-    fullscreen: false,
-    background: '#2196F3',
-    titleBarStyle: 'hiddenInset',
-    frame: process.platform == 'darwin'
-  });
+    mainWindow = new BrowserWindow({
+        show: false,
+        title: app.getName(),
+        width: 500,
+        height: 600,
+        minWidth: 500,
+        minHeight: 600,
+        maxWidth: 500,
+        maxHeight: 600,
+        resizable: false,
+        fullscreen: false,
+        background: '#2196F3',
+        titleBarStyle: 'hiddenInset',
+        frame: process.platform == 'darwin'
+    });
 
-  if (process.platform === 'darwin') {
-    mainWindow.setSheetOffset(40);
-  }
-
-  mainWindow.on('enter-full-screen', e => {
-    e.preventDefault();
-  });
-
-  mainWindow.on('ready-to-show', () => {
-      mainWindow.show();
-      mainWindow.focus();
-  });
-
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-
-  mainWindow.webContents.on('new-window', (e, url) => {
-    e.preventDefault();
-    electron.shell.openExternal(url);
-  });
-
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-
-  ygg.validate('token', function(err){
-    if (err) {
-      mainWindow.loadURL('file://' + __dirname + '/login.html');
+    if (process.platform === 'darwin') {
+        mainWindow.setSheetOffset(40);
     }
-  });
+
+    mainWindow.on('enter-full-screen', e => {
+        e.preventDefault();
+    });
+
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+        mainWindow.focus();
+    });
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+
+    mainWindow.webContents.on('new-window', (e, url) => {
+        e.preventDefault();
+        electron.shell.openExternal(url);
+    });
+
+    ygg.validate('token', function(err) {
+        if (err) {
+            mainWindow.loadURL('file://' + __dirname + '/views/login.pug');
+        } else {
+            mainWindow.loadURL('file://' + __dirname + '/views/index.pug');
+        }
+    });
 });
