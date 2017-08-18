8364a9aa13f00e8bc8e01644e7e6a0e25a02e1a7
Raúl Salas <rauls.martin@alumno.sek.es>
2017-08-18 02:00:49 +0200

    Some changes

--- index.js
+++ index.js
@@ -1,85 +1,105 @@
-'use strict';
+'use strict'
 
-const electron = require('electron');
-const ygg = require('litecraft-yggdrasil')({});
+const electron = require('electron')
+const ygg = require('litecraft-yggdrasil')({})
 
 require('electron-pug')({
-    pretty: true
-});
-require('electron-reload')(__dirname);
+  pretty: true
+})
+require('electron-reload')(__dirname, {
+  electron: require('${__dirname}/../../node_modules/electron')
+})
 
 const {
-    app,
-    BrowserWindow
-} = electron;
+  app,
+  BrowserWindow,
+  ipcMain
+} = electron
 
-let mainWindow;
+let onlineStatusWindow
+let mainWindow
 
 const isAlreadyRunning = app.makeSingleInstance(() => {
-    if (mainWindow) {
-        if (mainWindow.isMinimized()) {
-            mainWindow.restore();
-        }
-
-        mainWindow.show();
+  if (mainWindow) {
+    if (mainWindow.isMinimized()) {
+      mainWindow.restore()
     }
-});
+
+    mainWindow.show()
+  }
+})
 
 if (isAlreadyRunning) {
-    app.quit();
+  app.quit()
 }
 
 app.on('window-all-closed', () => {
-    if (process.platform != 'darwin') {
-        app.quit();
-    }
-});
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        show: false,
-        title: app.getName(),
-        width: 500,
-        height: 600,
-        minWidth: 500,
-        minHeight: 600,
-        maxWidth: 500,
-        maxHeight: 600,
-        resizable: false,
-        fullscreen: false,
-        background: '#2196F3',
-        titleBarStyle: 'hiddenInset',
-        frame: process.platform == 'darwin'
-    });
+  mainWindow = new BrowserWindow({
+    show: false,
+    title: app.getName(),
+    width: 500,
+    height: 600,
+    minWidth: 500,
+    minHeight: 600,
+    maxWidth: 500,
+    maxHeight: 600,
+    resizable: false,
+    fullscreen: false,
+    background: '#2196F3',
+    titleBarStyle: 'hiddenInset',
+    frame: process.platform == 'darwin'
+  })
 
-    if (process.platform === 'darwin') {
-        mainWindow.setSheetOffset(40);
-    }
+  mainWindow.loadURL('file://' + __dirname + '/views/main.pug')
 
-    mainWindow.on('enter-full-screen', e => {
-        e.preventDefault();
-    });
+  if (process.platform === 'darwin') {
+    mainWindow.setSheetOffset(40)
+  }
 
-    mainWindow.on('ready-to-show', () => {
-        mainWindow.show();
-        mainWindow.focus();
-    });
+  mainWindow.on('enter-full-screen', e => {
+    e.preventDefault()
+  })
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+    mainWindow.focus()
+  })
 
-    mainWindow.webContents.on('new-window', (e, url) => {
-        e.preventDefault();
-        electron.shell.openExternal(url);
-    });
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 
-    ygg.validate('holaraul', function(valid) {
+  mainWindow.webContents.on('new-window', (e, url) => {
+    e.preventDefault()
+    electron.shell.openExternal(url)
+  })
+
+  checkInternet(function(isConnected) {
+    if (isConnected) {
+      ygg.validate('holaraul', function(valid) {
         if (!valid) {
-            mainWindow.loadURL('file://' + __dirname + '/views/login.pug');
-            console.log(err);
-        } else {
-            mainWindow.loadURL('file://' + __dirname + '/views/index.pug');
+          mainWindow.loadURL('file://' + __dirname + '/views/login.pug')
+          console.log(valid)
         }
-    });
-});
+      })
+    } else {
+      console.log("Offline!")
+    }
+  })
+})
+
+function checkInternet(cb) {
+  require('dns').lookup('google.com', function(err) {
+    if (err && err.code == "ENOTFOUND") {
+      cb(false)
+    } else {
+      cb(true)
+    }
+  })
+}
