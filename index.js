98d92ded9ccafe421cd0dd08d6ed64a391de5b58
raulsmartin <rauls.martin@alumno.sek.es>
2017-08-22 15:06:44 +0200

    Commit

--- index.js
+++ index.js
@@ -4,102 +4,100 @@ const electron = require('electron')
 const ygg = require('litecraft-yggdrasil')({})
 
 require('electron-pug')({
-  pretty: true
+	pretty: true
 })
 require('electron-reload')(__dirname, {
-  electron: require('${__dirname}/../../node_modules/electron')
+	electron: require('${__dirname}/../../node_modules/electron')
 })
 
 const {
-  app,
-  BrowserWindow,
-  ipcMain
+	app,
+	BrowserWindow
 } = electron
 
-let onlineStatusWindow
 let mainWindow
 
 const isAlreadyRunning = app.makeSingleInstance(() => {
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) {
-      mainWindow.restore()
-    }
+	if (mainWindow) {
+		if (mainWindow.isMinimized()) {
+			mainWindow.restore()
+		}
 
-    mainWindow.show()
-  }
+		mainWindow.show()
+	}
 })
 
 if (isAlreadyRunning) {
-  app.quit()
+	app.quit()
 }
 
 app.on('window-all-closed', () => {
-  if (process.platform != 'darwin') {
-    app.quit()
-  }
+	if (process.platform != 'darwin') {
+		app.quit()
+	}
 })
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    show: false,
-    title: app.getName(),
-    width: 500,
-    height: 600,
-    minWidth: 500,
-    minHeight: 600,
-    maxWidth: 500,
-    maxHeight: 600,
-    resizable: false,
-    fullscreen: false,
-    background: '#2196F3',
-    titleBarStyle: 'hiddenInset',
-    frame: process.platform == 'darwin'
-  })
+	mainWindow = new BrowserWindow({
+		show: false,
+		title: app.getName(),
+		width: 500,
+		height: 600,
+		minWidth: 500,
+		minHeight: 600,
+		maxWidth: 500,
+		maxHeight: 600,
+		resizable: false,
+		fullscreen: false,
+		background: '#2196F3',
+		titleBarStyle: 'hiddenInset',
+		frame: process.platform == 'darwin'
+	})
 
-  mainWindow.loadURL('file://' + __dirname + '/views/main.pug')
+	mainWindow.loadURL('file://' + __dirname + '/views/main.pug')
 
-  if (process.platform === 'darwin') {
-    mainWindow.setSheetOffset(40)
-  }
+	if (process.platform === 'darwin') {
+		mainWindow.setSheetOffset(40)
+	}
 
-  mainWindow.on('enter-full-screen', e => {
-    e.preventDefault()
-  })
+	mainWindow.on('enter-full-screen', e => {
+		e.preventDefault()
+	})
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-    mainWindow.focus()
-  })
+	mainWindow.on('ready-to-show', () => {
+		mainWindow.show()
+		mainWindow.focus()
+	})
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
 
-  mainWindow.webContents.on('new-window', (e, url) => {
-    e.preventDefault()
-    electron.shell.openExternal(url)
-  })
+	mainWindow.webContents.on('new-window', (e, url) => {
+		e.preventDefault()
+		electron.shell.openExternal(url)
+	})
 
-  checkInternet(function(isConnected) {
-    if (isConnected) {
-      ygg.validate('holaraul', function(valid) {
-        if (!valid) {
-          mainWindow.loadURL('file://' + __dirname + '/views/login.pug')
-          console.log(valid)
-        }
-      })
-    } else {
-      console.log("Offline!")
-    }
-  })
+	checkInternet(function(isConnected) {
+		if (isConnected) {
+			ygg.validate('holaraul', function(valid) {
+				if (!valid) {
+					mainWindow.loadURL('file://' + __dirname + '/views/login.pug')
+					console.log(valid)
+				}
+			})
+		} else {
+			console.log('Offline!')
+		}
+	})
 })
 
 function checkInternet(cb) {
-  require('dns').lookup('google.com', function(err) {
-    if (err && err.code == "ENOTFOUND") {
-      cb(false)
-    } else {
-      cb(true)
-    }
-  })
+	require('dns').lookup('google.com', function(err) {
+		if (err && err.code == 'ENOTFOUND') {
+			cb(false)
+		} else {
+			cb(true)
+		}
+	})
 }
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,56 +1,54 @@
-const remote = require('electron').remote;
+const remote = require('electron').remote
 
 var dictionary = {
-        'help': {
-            'en': 'Help',
-            'es': 'Ayuda',
-            'ess': 'Ayudita',
-        }
-};
-var langs = ['en', 'es', 'ess'];
-var current_lang_index = 0;
-var current_lang = langs[current_lang_index];
+	'help': {
+		'en': 'Help',
+		'es': 'Ayuda',
+		'ess': 'Ayudita',
+	}
+}
+var langs = ['en', 'es', 'ess']
+var current_lang_index = 0
+var current_lang = langs[current_lang_index]
 
 window.change_lang = function() {
-    current_lang_index = ++current_lang_index % 3;
-    current_lang = langs[current_lang_index];
-    translate();
+	current_lang_index = ++current_lang_index % 3
+	current_lang = langs[current_lang_index]
+	translate()
 }
 
 function translate() {
-    $("[data-translate]").each(function(){
-        var key = $(this).data('translate');
-        $(this).html(dictionary[key][current_lang] || "N/A");
-    });
+	$('[data-translate]').each(function(){
+		var key = $(this).data('translate')
+		$(this).html(dictionary[key][current_lang] || 'N/A')
+	})
 }
 
 function dropdownList() {
-    document.getElementById("myDropdown").classList.toggle("show");
+	document.getElementById('myDropdown').classList.toggle('show')
 }
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName('dropdown-content')
+		for (var i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i]
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show')
+			}
+		}
+	}
 }
 
 window.onload = function() {
-  document.getElementById("version").innerHTML = remote.app.getVersion();
-  document.getElementById("minimize").addEventListener("click", function(e) {
-    remote.getCurrentWindow().minimize();
-  });
+	document.getElementById('version').innerHTML = remote.app.getVersion()
+	document.getElementById('minimize').addEventListener('click', function() {
+		remote.getCurrentWindow().minimize()
+	})
 
-  document.getElementById("close").addEventListener("click", function(e) {
-    remote.getCurrentWindow().close();
-  });
-  translate();
+	document.getElementById('close').addEventListener('click', function() {
+		remote.getCurrentWindow().close()
+	})
+	translate()
 }
